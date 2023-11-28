a72605f2e8a45159d59dedf279130ba89aed3a59
RICHARDAVF <richard.aviles@unmsm.edu.pe>
2023-11-28 11:18:55 -0500

    updates files smart

--- apirest/view/smartfood/urls.py
+++ apirest/view/smartfood/urls.py
@@ -2,7 +2,7 @@ from .guias.views import GuiasView,PDFFACTView,AnulacionGuiaView
 from django.urls import path
 urlpatterns = [
     path('v1/fact/',GuiasView.as_view()),
-    path('v1/pdf/<str:serie>/<str:num>/',PDFFACTView.as_view()),
+    path('v1/fact/pdf/<str:serie>/<str:num>/',PDFFACTView.as_view()),
     path('v1/guia/anulacion/<str:serie>/<str:numero>/',AnulacionGuiaView.as_view())
 
 ]
