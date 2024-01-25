4ded33388da930f1da46ed2d2526b0bd9354f53e
RICHARDAVF <richarferro1308@gmail.com>
2024-01-25 16:13:51 -0500

    se comento codigo que tiene weasyprint

--- apirest/view/smartfood/urls.py
+++ apirest/view/smartfood/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('v1/fact/',GuiasView.as_view()),
     path('v1/fact/pdf/<str:serie>/<str:num>/',PDFFACTView.as_view()),
     path('v1/guia/anulacion/<str:serie>/<str:numero>/',AnulacionGuiaView.as_view()),
-    path('v1/pdf/gre/<str:serie>/<str:numero>/',SFPDFGenerate.as_view())
+    # path('v1/pdf/gre/<str:serie>/<str:numero>/',SFPDFGenerate.as_view())
 ]
